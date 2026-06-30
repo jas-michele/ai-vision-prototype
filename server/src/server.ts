771a0a348294161e8f71449aaddf8c634860be88
Jasmine <jasmineoliver158@gmail.com>
2026-06-30 11:13:01 -0400

    Setup Express backend and vision route

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import visionRoutes from "./routes/visionRoutes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/vision", visionRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
